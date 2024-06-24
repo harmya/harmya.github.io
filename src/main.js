c66fcc56ef6728ef8b4f9a53979a9bf371506cdf
harmya <hvbhatt@purdue.edu>
2024-06-24 02:42:12 -0400

    cooking

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import VueAnimateOnScroll from 'vue-animate-onscroll'
-import 'animate.css';
-import VueGlide from 'vue-glide-js'
-
-createApp(App).use(VueAnimateOnScroll).use(VueGlide).mount('#app')
