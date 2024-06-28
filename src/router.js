72a37574d43a1b2b257fa5d7d1e7c9bc3e01a0d5
harmya <hvbhatt@purdue.edu>
2024-06-28 09:40:02 -0400

    cooked

--- src/router.js
+++ src/router.js
@@ -3,22 +3,21 @@ import HomePage from './components/HomePage.vue';
 import FriendsPage from './components/FriendsPage.vue';
 
 const routes = [
-    {
-        path: '/friends',
-        name: 'FriendsPage',
-        component: FriendsPage,
-    },
+  {
+    path: '/friends',
+    name: 'FriendsPage',
+    component: FriendsPage,
+  },
 
   {
     path: '/',
     name: 'HomePage',
     component: HomePage,
   },
-
 ];
 
 const router = new createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory('/'),
   routes,
 });
 
