783fb19cdc64b94dc6e66100e64930e282c425b0
harmya <hvbhatt@purdue.edu>
2024-07-27 00:37:00 -0400

    updating git ignore

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import VueAnimateOnScroll from 'vue-animate-onscroll';
+import 'animate.css';
+import router from './router';
+
+createApp(App).use(VueAnimateOnScroll).use(router).mount('#app');
