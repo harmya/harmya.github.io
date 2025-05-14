46337e2eda5308357a7abe259a82e981e5863149
harmya <hvbhatt@purdue.edu>
2025-05-14 01:41:21 -0400

    cooked

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,26 @@
+// svelte.config.js
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
 	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: 'index.html',  // This is important for SPA mode
 			precompress: false,
-			strict: true
+			strict: false  // This allows us to ignore the dynamic route warning
 		})
 	}
 };
 
+export default config;
