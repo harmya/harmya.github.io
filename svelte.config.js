9545b703c3d2912074ef96ce03318980989585cf
harmya <hvbhatt@purdue.edu>
2025-05-14 01:10:17 -0400

    cooked

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
+// Make sure this is exactly how your svelte.config.js looks
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: '' // Empty for user pages (username.github.io)
+		}
 	}
 };
 
