d41611234dfe22138de09935676fc97b7fe2ae1f
harmya <hvbhatt@purdue.edu>
2025-05-14 01:24:27 -0400

    guh

--- svelte.config.js
+++ svelte.config.js
@@ -18,7 +18,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/harmya.github.io' : ''
+			base: process.env.NODE_ENV === 'production' ? '/' : ''
 		}
 	}
 };
